053ab2447f6e018ff82c0e5b6055e7ca2b003d06
ralphkakes <99045360@mydavinci.nl>
2020-02-18 14:17:49 +0100

    Working start button

--- stemwijzer.js
+++ stemwijzer.js
@@ -1,7 +1,16 @@
 let counter = 0;
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var startButton = document.getElementById("startButton");
 
 function start(){
-    console.log(subjects[counter];
+    button1.style.display="inline";
+    button2.style.display="inline";
+    button3.style.display="inline";
+    startButton.style.display="none";
+    console.log("test");
+
 }
 
 
