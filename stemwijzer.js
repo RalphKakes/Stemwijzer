5cdf189cbe6800751017f01bed862acb883bd94d
ralphkakes <99045360@mydavinci.nl>
2020-03-10 23:58:17 +0100

    Massive Do-over

--- stemwijzer.js
+++ stemwijzer.js
@@ -1,31 +1,62 @@
-let counter = 0;
-var button1 = document.getElementById("button1");
-var button2 = document.getElementById("button2");
-var button3 = document.getElementById("button3");
-var startButton = document.getElementById("startButton");
+const stellingTitle = document.getElementById("stellingTitle");
+const stellingStatement = document.getElementById("stellingStatement");
+const stellingParties = document.getElementById('parties');
+const buttons = document.getElementById('buttons');
+const buttonBack = document.getElementById('buttonBack');
+const multiply = document.getElementById('multiply');
 
-function start(){
-    button1.style.display="inline";
-    button2.style.display="inline";
-    button3.style.display="inline";
-    startButton.style.display="none";
-    console.log("test");
+var  answers = [];
+
+var  counter  = 0;
+
+document.getElementById("start").addEventListener("click", function start(){
+    document.getElementById("titels").style.display = 'none';
+    document.getElementById("footer").style.display = 'none';
+
+    stellingStatement.classList.remove('invisible');
+    stellingParties.classList.add('mt-5');
+    stellingParties.classList.remove('invisible');
+    buttons.classList.remove('invisible');
+    buttonBack.classList.remove('invisible');
+    document.getElementById('multiplyDiv').classList.remove('invisible');
+
+    for (let i = 0; i < parties.length; i++) {
+        if (isNaN(parties[i].points)){
+            parties[i].points = 0
+        }
+    }
+    getStelling()
+});
+
+function getStelling(){
+    if(answers.length !== subjects.length){
+        stellingTitle.innerHTML = questionCount + 1 + '. ' + subjects[questionCount].title
+        stellingStatement.innerHTML = subjects[questionCount].statement
+        document.getElementById("bar").style.width = 14.2857142857 * (questionCount + 1) + "%";
+
+        document.getElementById("eens").innerHTML = "";
+        document.getElementById("geen van beide").innerHTML = "";
+        document.getElementById("oneens").innerHTML = "";
+
+        subjects[questionCount].parties.forEach(function(element) {
+            if (element.position == "pro") {
+                document.getElementById("eens").innerHTML += "<details class=\"opinion__party\"><summary class=\"party__title\">" + element.name + "</summary><p class=\"party__description\"> " + element.explanation + "</p></details>"
+            } else if (element.position == "ambivalent") {
+                document.getElementById("none").innerHTML += "<details class=\"opinion__party\"><summary class=\"party__title\">" + element.name + "</summary><p class=\"party__description\"> " + element.explanation + "</p></details>"
+            } else if (element.position == "contra") {
+                document.getElementById("oneens").innerHTML += "<details class=\"opinion__party\"><summary class=\"party__title\">" + element.name + "</summary><p class=\"party__description\"> " + element.explanation + "</p></details>"
+            }
+        });
+    } else{
+        showResult();
+    }
 }
 
-
- /* function matchParty(){
-    for (let counter = 0; counter < pro.length; counter++){
-        pro[counter].score = 0;
+function goBack(){
+    if(questionCount === 0){
+        location.href = "/"
+    } else{
+        questionCount--;
+        getStelling();
     }
-    for (let partyNr = 0; partyNr < answers;.length; partyNr++){
-        console.log()
-    }
-    for (let counter = 0; counter < parties[partyNr].pro.length; counter++){
-
-    }
-} */
-
-// if my like for this meal is equal to the mealfriend its likes then//
-
-// find corresponding element in friends en increment its score//
-
+}
