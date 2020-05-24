930b36a2b717f91a915ea1d2e47a3198557c7a4c
ralphkakes <99045360@mydavinci.nl>
2020-05-24 02:07:44 +0200

    Added show secular and show big boy parties

--- stemwijzer.js
+++ stemwijzer.js
@@ -4,33 +4,38 @@ const stellingParties = document.getElementById('parties');
 const buttons = document.getElementById('buttons');
 const buttonBack = document.getElementById('buttonBack');
 const multiply = document.getElementById('multiply');
+const onlySecular = document.getElementById('onlySecular')
+const onlyBig = document.getElementById('onlyBig')
+const minSize = 18;
 var questionCount = 0;
+
 
 var  answers = [];
 
 parties.forEach(element =>element.points = 0)
 
 document.getElementById("start").addEventListener("click", function start(){
+    document.getElementById("buttons").style.display = 'block';
     document.getElementById("titles").style.display = 'none';
     document.getElementById("footer").style.display = 'none';
 
     stellingStatement.classList.remove('invisible');
-    buttons.classList.remove('invisible');
     buttonBack.classList.remove('invisible');
 
     getStelling()
 });
 
 function getStelling(){
-    console.log('ANSWERS LENGTH:' + answers.length);
-    console.log('SUBJECTS LENGTH:' + subjects.length);
-    console.log('Answers array' + answers);
+    // console.log('ANSWERS LENGTH:' + answers.length);
+    // console.log('SUBJECTS LENGTH:' + subjects.length);
+    // console.log('Answers array' + answers);
     multiply.checked = false;
     if(answers.length !== subjects.length){
         stellingTitle.innerHTML = questionCount + 1 + '. ' + subjects[questionCount].title
         stellingStatement.innerHTML = subjects[questionCount].statement
     } else{
-        results();
+        document.getElementById("buttons").style.display = 'none';
+        calculateResult();
     }
 }
 
@@ -87,18 +92,76 @@ function changeStelling(opinion) {
     getStelling();
 }
 
-function results(){
+// Calculates the results based on the settings you entered 
+function calculateResult(){
+    // Looks if both big parties and secular is turned on
+    if(onlyBig.checked && onlySecular.checked){
+        showResult(getBigAndSecular())
+    }
+    // checks if only Big
+    else if(onlyBig.checked){
+        showResult(getBigParty())
+    }    //checks if only secular
+   
+    else if(onlySecular.checked){
+        showResult(getSecular())
+    } else{
+        showResult(parties)
+    }
+}
+
+
+// Shows result from the showResult() function
+function showResult(part){
     var div = document.getElementById("results")
     var result = document.createElement("span")
     var length = 100/subjects.length
-    for (let i = 0; i < parties.length;  i++) {
-        var lengthbar = length * parties[i].points
-        // gives percentage of party
-        result.innerHTML += '<p class="mb-0 mt-1">' + parties[i].name + '</p>' + '<div style="max-width:1000px;width:'+ lengthbar +'%">'+ Math.round(lengthbar) + '%</div>';
+    for (let i = 0; i < part.length; i++) {
+        var lengthbar = length * part[i].points
+        result.innerHTML += '<p class="mb-0 mt-1">' + part[i].name + '</p>' + '<div style="max-width:1000px;width:'+ lengthbar +'%">'+ Math.round(lengthbar) + '%</div>'; 
     }
     div.appendChild(result)
 }
 
+
+
+// Gets parties where secular is true
+function getSecular(){
+    var arr = []
+    for (let i = 0; i < parties.length; i++) {
+        if(parties[i].secular === true){
+            arr.push(parties[i])
+        }
+    }
+    return arr
+}
+
+
+// Gets parties where big party is is higer than the minsize
+function getBigParty(){
+    var arr = []
+    for (let i = 0; i < parties.length; i++) {
+        if(parties[i].size > minSize){
+            arr.push(parties[i])
+        }
+    }
+    return arr
+}
+
+
+// Gets parties where secular is true and the parties is bigger than the minsize
+function getBigAndSecular(){
+    var arr = []
+    for (let i = 0; i < parties.length; i++) {
+        if(parties[i].secular === true && parties[i].size > minSize){
+            arr.push(parties[i])
+        }
+    }
+    return arr
+}
+
+
+// Go back ;)
 function goBack(){
     if(questionCount === 0){
         location.reload();
